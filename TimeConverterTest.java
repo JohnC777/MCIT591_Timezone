9f4972cf02bce04292843adfaeccc0adc2cf1c85
JohnC777 <45903691+JohnC777@users.noreply.github.com>
2019-04-21 22:14:48 -0400

    Add files via upload

---
+++ TimeConverterTest.java
@@ -0,0 +1,24 @@
+package application;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TimeConverterTest {
+
+	@Test
+	void testTimeConverter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testConvert() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
