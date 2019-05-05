69701ca2311b49b4893be7d9f2200907d350b9bf
cpenn <49132727+cpenn@users.noreply.github.com>
2019-05-05 17:57:33 +0930

    updated after new changes in TimeConverter.java

--- TimeConverterTest.java
+++ TimeConverterTest.java
@@ -1,24 +1,16 @@
-package application;
-
 import static org.junit.jupiter.api.Assertions.*;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 import org.junit.jupiter.api.Test;
 
 class TimeConverterTest {
 
 	@Test
-	void testTimeConverter() {
-		fail("Not yet implemented");
-	}
-
-	@Test
 	void testConvert() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testMain() {
-		fail("Not yet implemented");
+		TimeConverter tc1 = new TimeConverter(); 
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		assertEquals(tc1.Convert("2018-05-05 16:00:00", "US/Eastern", "Australia/Adelaide").format(formatter), "2018-05-06 05:30:00"); 
 	}
 
 }
