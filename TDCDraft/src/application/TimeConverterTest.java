87b32317e5f6cdce0930e26231c8d22ad5e86b8c
Merriad <49375740+Merriad@users.noreply.github.com>
2019-05-11 20:11:34 -0400

    Updating TimeConverterTest.java

--- TDCDraft/src/application/TimeConverterTest.java
+++ TDCDraft/src/application/TimeConverterTest.java
@@ -1,16 +1,97 @@
+package application;
+
 import static org.junit.jupiter.api.Assertions.*;
-import java.time.ZonedDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import org.junit.jupiter.api.Test;
 import java.time.format.DateTimeFormatter;
 
-import org.junit.jupiter.api.Test;
-
 class TimeConverterTest {
-
+	
+	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+	
+	/**
+	 * Testing Convert Method: various functionalities 
+	 */
+	TimeConverter tc1 = new TimeConverter();
+	
 	@Test
-	void testConvert() {
-		TimeConverter tc1 = new TimeConverter(); 
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-		assertEquals(tc1.Convert("2018-05-05 16:00:00", "US/Eastern", "Australia/Adelaide").format(formatter), "2018-05-06 05:30:00"); 
+	void testConvertAdelaide() {
+		LocalDate date = LocalDate.of(2019, 5, 8);
+		LocalTime time = LocalTime.of(18, 30);
+		assertEquals(tc1.Convert(date, time, "US/Eastern", "Australia/Adelaide"), "2019-05-09 08:00"); 
 	}
+	// Cross International Date Line (+1 day)
+	
+	@Test
+	void testConvertSanFrancisco() {
+		LocalDate date = LocalDate.of(2019, 1, 1);
+		LocalTime time = LocalTime.of(0, 0);
+		assertEquals(tc1.Convert(date, time, "US/Eastern", "US/Pacific"), "2018-12-31 21:00");
+	}
+	// working with midnight 
+	
+	@Test
+	void testConvertLondon() {
+		LocalDate date = LocalDate.of(2019, 10, 31);
+		LocalTime time = LocalTime.of(23, 59);
+		assertEquals(tc1.Convert(date, time, "Europe/London", "US/Eastern"), "2019-10-31 19:59");
+	}
+	// same day 
+	
+	@Test
+	void testConvertTokyo() {
+		LocalDate date = LocalDate.of(2019, 4, 1);
+		LocalTime time = LocalTime.of(6, 45);
+		assertEquals(tc1.Convert(date, time, "US/Eastern", "Asia/Tokyo"), "2019-04-01 19:45");
+	}
+	
+	@Test
+	void testConvertTehran() {
+		LocalDate date = LocalDate.of(2019, 11, 11);
+		LocalTime time = LocalTime.of(3, 0);
+		assertEquals(tc1.Convert(date, time, "Asia/Tehran", "America/Santiago"), "2019-11-10 20:30");
+	}
+	// Cross International Date Line (-1 day)
+	
+	/**
+	 * Test OutputFormatterDatePicker Method
+	 */
+	@Test
+	void testOutputFormatterDatePicker1() {
+		String input = "2019-05-31 18:30";
+		tc1.outputFormatterDatePicker(input);
+		assertEquals(tc1.getNewYear(), 2019);
+		assertEquals(tc1.getNewMonth(), 5);
+		assertEquals(tc1.getNewDay(), 31);
+	}
+	
+	@Test
+	void testOutputFormatterDatePicker2() {
+		String input = "2019-12-01 00:00";
+		tc1.outputFormatterDatePicker(input);
+		assertEquals(tc1.getNewYear(), 2019);
+		assertEquals(tc1.getNewMonth(), 12);
+		assertEquals(tc1.getNewDay(), 1);
+	}
+	
+	
+	/**
+	 * Test OutputFormatterTimeSpinner Method
+	 */
+	
+	@Test
+	void testOutputFormatterTimeSpinner1() {
+		String input = "2019-01-01 09:12";
+		LocalTime time = LocalTime.of(9, 12);
+		assertEquals(tc1.outputFormatterTimeSpinner(input), time);
+	}
+	
+	void testOutputFormatterTimeSpinner2() {
+		String input = "2019-12-01 11:03";
+		LocalTime time = LocalTime.of(11, 3);
+		assertEquals(tc1.outputFormatterTimeSpinner(input), time);
+	}
+
 
 }
