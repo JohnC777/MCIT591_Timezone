c5da424766391d7537da8f6767ce72eec3ae52c7
Merriad <49375740+Merriad@users.noreply.github.com>
2019-05-12 20:43:49 -0400

    Updating DropDownMenu.java

--- TDCDraft/src/application/DropDownMenu.java
+++ TDCDraft/src/application/DropDownMenu.java
@@ -12,7 +12,7 @@ import javafx.scene.control.ComboBox;
  *
  */
 public class DropDownMenu {
-	ComboBox menuComboBox;
+	ComboBox<String> menuComboBox;
 
 	/**
 	 * Method to create a ComboBox with all the city/time zone options to display to
@@ -32,13 +32,13 @@ public class DropDownMenu {
 				"Houston (UTC-05:00)", "Jakarta (UTC+07:00)", "Kathmandu (UTC+05:45)", "Kinshasa (UTC+01:00)",
 				"Lahore (UTC+05:00)", "Lima (UTC-05:00)", "London (UTC+01:00)", "Los Angeles (UTC-07:00)",
 				"Magadan (UTC+10:00)", "Mexico City (UTC-05:00)", "Mumbai (UTC+05:30)", "New York (UTC-04:00)",
-				"Nuuk (UTC-02:00)", "Perth (UTC+08:00)", "Reykjavik (UTC+00:00)", "Rome (UTC+02:00)",
-				"Salt Lake City (UTC-06:00)", "Santiago (UTC-03:00)", "Sao Paulo (UTC-03:00)", "Sydney (UTC+10:00)",
-				"Tehran (UTC+04:30)", "Tokyo (UTC+09:00)", "Toronto (UTC-04:00)", "Vancouver (UTC-07:00)",
-				"Yangon (UTC+06:30)");
+				"Nuuk (UTC-02:00)", "Perth (UTC+08:00)", "Philadelphia (UTC-04:00)", "Reykjavik (UTC+00:00)",
+				"Rome (UTC+02:00)", "Salt Lake City (UTC-06:00)", "Santiago (UTC-03:00)", "Sao Paulo (UTC-03:00)",
+				"Sydney (UTC+10:00)", "Tehran (UTC+04:30)", "Tokyo (UTC+09:00)", "Toronto (UTC-04:00)",
+				"Vancouver (UTC-07:00)", "Yangon (UTC+06:30)");
 
 		// Constructing combo box.
-		menuComboBox = new ComboBox(menuOptions);
+		menuComboBox = new ComboBox<String>(menuOptions);
 		menuComboBox.setItems(menuOptions);
 
 		// Restricts viewing up to 7 options at a time.
